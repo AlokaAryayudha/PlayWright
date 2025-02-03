bb7f3d2884938fb3d37d7aaafb4a54745407792a
AlokaAryayudha <Aaloka221@gmail.com>
2025-02-03 15:02:40 +0700

    update issues

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -5,9 +5,17 @@ import { getLoginToken } from "../api-calls/getLoginToken"
 import { adminDetails } from "../data/userDetails"
 import { log } from "console"
 
-test("My account useing cookie injection", async ({page}) => {
-    const loginToken = await getLoginToken()
-    console.warn({loginToken})
+test("My Account using cookie injection and mocking network request", async ({ page }) => {
+    const loginToken = await getLoginToken(adminDetails.username, adminDetails.password)
+
+    await page.route("**/api/user**", async (route, request) => {
+        await route.fulfill({
+            status: 500,
+            contentType: "application/json",
+            body: JSON.stringify({message: "PLAYWRIGHT ERROR FROM MOCKING"}),
+        })
+    })
+    
     const myAccount = new MyAccountPage(page)
     await myAccount.visit()
     await page.evaluate(([loginTokenInsideBrowserCode]) => {
@@ -15,24 +23,5 @@ test("My account useing cookie injection", async ({page}) => {
     }, [loginToken])
     await myAccount.visit()
     await myAccount.waitForPageHeading()
+    await myAccount.waitForErrorMessage()
 })
-
-// test("My Account using cookie injection and mocking network", async ({page}) => {
-//     const loginToken = await getLoginToken(adminDetails.username, adminDetails.password)
-
-    // await page.route("**/api/user**", async (route, request) =>{
-    //     await route.fulfill({
-    //     status: 500,
-    //     contentType: "application/json",
-    //     body: JSON.stringify({message: "PLAYRIGHT ERROR FROM MOCKING"}),
-    // })
-    // })
-    
-    // const myAccount = new MyAccountPage(page)
-    // await myAccount.visit()
-    // await page.evaluate(([loginTokenInsideBrowserCode]) => {
-    //     document.cookie = "token=" + loginTokenInsideBrowserCode
-    // }, [loginToken])
-    // await myAccount.visit()
-    // await myAccount.waitForPageHeading()
-    // await myAccount.waitForErrorMessage()
