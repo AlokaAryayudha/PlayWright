6a30c51a377f28cd642647258b1b99e90490d42a
AlokaAryayudha <Aaloka221@gmail.com>
2025-02-03 11:50:36 +0700

    update issues

--- api-calls/getLoginToken.js
+++ api-calls/getLoginToken.js
@@ -4,7 +4,7 @@ import * as nodeFetch from "node-fetch"
 export const getLoginToken = async (username, password) => {
     const response = await nodeFetch("http://localhost:2221/api/login",{
         method: "POST",
-        body: JSON.stringify({"username": username, "password": password }),
+        body: JSON.stringify({"username":"admin","password":"Admin123" }),
     })
     if (response.status !== 200){
         throw new error("An error occured trying to retrieve the login token.")
